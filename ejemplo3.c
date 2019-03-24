e53b8592b6ba67278a1fc941a7b732b51cfadcb3
JesusVal <is703557@iteso.mx>
2019-03-24 16:17:06 -0600

    Se agrego ejemplo3 incompleto o piloto

---
+++ ejemplo3.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+
+#include "misemaforo.h"
+
+#define atomic_xchg(A,B)	__asm__ __volatile__ (  \
+                                    "lock xchg %1,%0 ;\n"  \
+                                    : "=ir" (A)            \
+                                    : "m" (B), "ir" (A)    \
+                                    );
+
+#define CICLOS 10
+
+char *pais[3] = {"Peru", "Bolivia", "Colombia"};
+
+SEMAFORO *sem;
+
+int *g;
+
+void proceso(int i);
+
+int main(int argc, char const *argv[])
+{
+	int pid;
+	int status;
+	int i;
+	int idsem;
+
+	//Solicitar memoria compartida
+	idsem = shmget(IPC_PRIVATE, sizeof(SEMAFORO), IPC_CREAT | 0666);
+
+	// Conectar la variable a la memoria compartida
+	sem = (SEMAFORO*) shmat(idsem, NULL, 0);
+
+	//Se usa el id como semilla
+	srand(getpid());
+
+	//Inicializa el semaforo
+	initsem(sem, 1);
+
+	for( i = 0 ; i < 3 ; i++ )
+	{
+		//Crea un nuevo proceso hijo que ejecuta la función proceso()
+		pid = fork();
+		if( pid == 0 )
+			proceso(i);
+	}
+
+	for( i = 0 ; i < 3 ; i++ )
+		pid = wait(&status);
+
+	//Eliminar memoria compartida
+	shmdt(g);
+	/* code */
+	return 0;
+}
+
+void proceso(int i)
+{
+	int k;
+
+	for( k = 0 ; k < CICLOS ; k++)
+	{
+		//Llamada waitsem implementada en la parte 3
+		//waitsem(sem);
+
+		//Sección crítica
+		printf("*%s Entra\n", pais[i]);
+		fflush(stdout);
+		sleep(rand()%3);
+		printf("-%s Sale\n", pais[i]);
+
+		//Llamada waitsignal implementada en la parte 3
+		//signalsem(sem);
+
+		//Espera aleatoria fuera de la seccón cŕitica
+		sleep(rand()%3);
+	}
+
+
+	exit(0);
+}
